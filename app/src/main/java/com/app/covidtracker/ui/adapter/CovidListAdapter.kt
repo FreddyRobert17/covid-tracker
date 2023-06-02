bdf2c236dd681999d704aff44faf43c727da0c7a
FreddyRobert17 <freddy.bedregal.ticona@gmail.com>
2023-06-02 12:39:45 -0500

    feat: add adapter and list item

---
+++ app/src/main/java/com/app/covidtracker/ui/adapter/CovidListAdapter.kt
@@ -0,0 +1,42 @@
+package com.app.covidtracker.ui.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.app.covidtracker.data.network.CovidDailyData
+import com.app.covidtracker.databinding.CovidListItemBinding
+
+class CovidListAdapter: ListAdapter<CovidDailyData, CovidListAdapter.CovidViewHolder>(DiffCallback) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CovidViewHolder {
+        val binding = CovidListItemBinding.inflate(LayoutInflater.from(parent.context))
+        return CovidViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: CovidViewHolder, position: Int) {
+        val covidDailyData = getItem(position)
+        holder.bind(covidDailyData)
+    }
+
+    inner class CovidViewHolder(private val binding: CovidListItemBinding): RecyclerView.ViewHolder(binding.root){
+        fun bind(covidDailyData: CovidDailyData){
+            binding.tvDate.text = covidDailyData.date.toString()
+            binding.tvPositives.text = covidDailyData.positives.toString()
+            binding.tvNegatives.text = covidDailyData.negatives.toString()
+            binding.tvHospitalized.text = covidDailyData.hospitalized.toString()
+            binding.tvDead.text = covidDailyData.death.toString()
+        }
+    }
+
+    companion object DiffCallback: DiffUtil.ItemCallback<CovidDailyData>(){
+        override fun areItemsTheSame(oldItem: CovidDailyData, newItem: CovidDailyData): Boolean {
+            return oldItem.date == newItem.date
+        }
+
+        override fun areContentsTheSame(oldItem: CovidDailyData, newItem: CovidDailyData): Boolean {
+            return oldItem == newItem
+        }
+    }
+}
