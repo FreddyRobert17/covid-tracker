442eb21d5e1bdda1403d9313d5b78366fdbc07a8
FreddyRobert17 <freddy.bedregal.ticona@gmail.com>
2023-06-01 21:07:28 -0500

    chore: add kotlin activity dependency

--- app/src/main/java/com/app/covidtracker/MainActivity.kt
+++ app/src/main/java/com/app/covidtracker/MainActivity.kt
@@ -2,10 +2,23 @@ package com.app.covidtracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.viewModels
+import androidx.lifecycle.Observer
+import com.app.covidtracker.viewmodel.CovidViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    val viewmodel: CovidViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewmodel.covidModel.observe(this, Observer {
+            Log.i("TAG", it.toString())
+        })
     }
 }
